f7cfd7356c82630d8c96e1052227637de8545694
viandwi24 <fiandwi0424@gmail.com>
2020-08-16 00:12:49 +0700

    [UPDATE] admin schema crud slug id

--- pages/admin/skema/_id.vue
+++ pages/admin/skema/_id.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    {{ skema.title }}
+    <div class="content-header">
+      <h1 class="text-4xl text-gray-800">
+        {{ skema.title }}
+      </h1>
+      <tw-breadcrumb :items="breadcrumbs" />
+    </div>
   </div>
 </template>
 
@@ -15,15 +20,34 @@ export default {
     const id = params.id
     let skema
 
+    app.$overlayLoading.show()
     try {
       skema = (await read(id)).data
     } catch (error) {
       await redirect({ name: 'admin-skema' })
     }
+    await app.$sleep(500)
+    app.$overlayLoading.hide()
+
+    // bc
+    const breadcrumbs = [
+      { text: 'Home', route: 'admin' },
+      { text: 'Skema', route: 'admin-skema' },
+      { text: skema.title } //, route: { name: 'admin-skema-id', params: { id } }
+    ]
 
     return {
       id,
-      skema
+      skema,
+      breadcrumbs
+    }
+  },
+  layout: 'dashboard',
+  middleware: ['auth', 'is_admin'],
+  transition: 'dashboard',
+  head () {
+    return {
+      title: `Dashboard - Admin - Skema - ${this.skema.title}`
     }
   }
 }
