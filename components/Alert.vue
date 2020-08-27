b58788da8a2ab383c840db71f5b8885fd0fb97eb
viandwi24 <fiandwi0424@gmail.com>
2020-08-27 23:27:40 +0700

    Add alert component

---
+++ components/Alert.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="alert" :class="alert" role="alert">
+    <div v-if="alert === 'left-accent-border'" class="border-l-4 p-4" :class="`${type} ${classType}`">
+      <slot />
+    </div>
+    <div v-else :class="`${type} ${classType}`">
+      <slot />
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from '@vue/composition-api'
+export default {
+  props: {
+    alert: {
+      type: String,
+      default: 'normal'
+    },
+    type: {
+      type: String,
+      default: 'primary'
+    }
+  },
+  setup (props, { app }) {
+    const classType = ref('')
+
+    // const
+    return {
+      classType
+    }
+  }
+}
+</script>
